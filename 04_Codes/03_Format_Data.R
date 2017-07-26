388133860b8487503ae295f782d1e886dc5c942b
HeraclesHX <huijijiyi@gmail.com>
2017-07-26 11:02:06 +0800

    move the project inrtroduction to the Background Folder
    Move the project introduction to the Background folder and add the 03_Format_Data.R in the Codes folder

---
+++ 04_Codes/03_Format_Data.R
@@ -0,0 +1,9 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ProjectName:  Xi'an Jassen PET
+# Purpose:      Format the data for the plot described in PPT
+# programmer:   Xin Huang
+# Date:         06-20-2017
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+
